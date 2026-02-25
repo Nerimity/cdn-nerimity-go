37ee260ddd65a1e3d12114ec3df12d3515a44709
SupertigerDev <saawalkhn@googlemail.com>
2026-02-25 10:14:34 +0000

    refactor code

--- handlers/content.go
+++ handlers/content.go
@@ -14,63 +14,26 @@ import (
 )
 
 func GetContentHandler(c fiber.Ctx) error {
-	urlPath := c.Path()
-
-	decodedBasename, err := url.PathUnescape(path.Base(urlPath))
+	finalPath, err := resolveSafePath(c.Path())
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid encoding")
-	}
-
-	// Force path to be relative to prevent escaping to root
-	relPath := path.Join(path.Dir(urlPath), decodedBasename)
-
-	relPath = strings.TrimPrefix(relPath, "/")
-
-	baseDir := "./public"
-	finalPath := filepath.Join(baseDir, relPath)
-
-	absBase, _ := filepath.Abs(baseDir)
-	absFinal, err := filepath.Abs(finalPath)
-	if err != nil || !strings.HasPrefix(absFinal, absBase) {
 		return c.Status(fiber.StatusForbidden).End()
 	}
 
-	ext := filepath.Ext(finalPath)
 	// Check file size
 	info, err := os.Stat(finalPath)
-	if err != nil {
+	if err != nil || info.IsDir() {
 		return c.Status(fiber.StatusNotFound).End()
 	}
-	if info.IsDir() {
-		return c.Status(fiber.StatusNotFound).End()
-	}
-	fileSizeMB := info.Size() / (1024 * 1024) // size in MB
-	var isImageFileSize = fileSizeMB <= 20
 
-	if utils.IsImage(strings.ToLower(ext)) && isImageFileSize {
-		imageType := c.Query("type")
-		size := c.Query("size")
-		if size != "" || imageType != "" {
-			var static = imageType == "webp"
-			var parsedSize = 0
-			if size != "" {
-				parsedSize, err = strconv.Atoi(size)
-				if err != nil {
-					parsedSize = 0
-				}
-			}
-			var url = utils.GenerateImageProxyURL(utils.ImageProxyOptions{URL: finalPath, IsLocalURL: true, Static: static, Size: parsedSize})
-			println("Processing Image", url)
-
-			return proxy.Do(c, url)
-		}
+	if shouldProxyImage(c, finalPath, info.Size()) {
+		return handleProxyImage(c, finalPath)
 	}
 
 	println("Serving file:", finalPath)
-	return ServeFile(c, finalPath)
+	return serveFile(c, finalPath)
 }
 
-func ServeFile(c fiber.Ctx, finalPath string) error {
+func serveFile(c fiber.Ctx, finalPath string) error {
 	ext := strings.ToLower(filepath.Ext(finalPath))
 
 	switch {
@@ -87,4 +50,50 @@ func ServeFile(c fiber.Ctx, finalPath string) error {
 	default:
 		return c.Download(finalPath)
 	}
+}
+
+func resolveSafePath(urlPath string) (string, error) {
+	decodedBasename, err := url.PathUnescape(path.Base(urlPath))
+	if err != nil {
+		return "", err
+	}
+
+	// Force path to be relative to prevent escaping to root
+	relPath := path.Join(path.Dir(urlPath), decodedBasename)
+
+	relPath = strings.TrimPrefix(relPath, "/")
+
+	baseDir := "./public"
+	finalPath := filepath.Join(baseDir, relPath)
+
+	absBase, _ := filepath.Abs(baseDir)
+	absFinal, err := filepath.Abs(finalPath)
+	if err != nil || !strings.HasPrefix(absFinal, absBase) {
+		return "", os.ErrPermission
+	}
+	return finalPath, nil
+}
+
+func shouldProxyImage(c fiber.Ctx, finalPath string, size int64) bool {
+	ext := strings.ToLower(filepath.Ext(finalPath))
+	fileSizeMB := size / (1024 * 1024) // size in MB
+	var isImageFileSize = fileSizeMB <= 20
+	var hasTransformParams = c.Query("size") != "" || c.Query("type") != ""
+
+	return utils.IsImage(ext) && isImageFileSize && hasTransformParams
+
+}
+
+func handleProxyImage(c fiber.Ctx, finalPath string) error {
+	imageType := c.Query("type")
+	size := c.Query("size")
+	var static = imageType == "webp"
+	var parsedSize = 0
+	if size != "" {
+		parsedSize, _ = strconv.Atoi(size)
+	}
+	var proxyURL = utils.GenerateImageProxyURL(utils.ImageProxyOptions{URL: finalPath, IsLocalURL: true, Static: static, Size: parsedSize})
+	println("Processing Image", proxyURL)
+
+	return proxy.Do(c, proxyURL)
 }
